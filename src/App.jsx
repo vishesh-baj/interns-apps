443baa0ad73ab817071fc4aa057c99ff998efe05
radhaNagr <radha.tophat@gmail.com>
2023-04-13 14:13:53 +0530

    adding login

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import "./App.css"
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Form } from "react-bootstrap";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+
+      <div className='container'>
+        <h1 id="lg">Login Form</h1>
+        <Form>
+          <Form.Group className="mb-3">
+            <Form.Label className="label">Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label className="label">Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3">
+          </Form.Group>
+          <Button variant="success" type="submit">
+            Log in
+          </Button>
+        </Form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  );
+    </>
+  )
 }
+
 
 export default App;
