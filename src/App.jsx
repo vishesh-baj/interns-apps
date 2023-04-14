5312651c9efa0451bf1444831568ef54f82c1404
radhaNagr <radha.tophat@gmail.com>
2023-04-14 11:37:31 +0530

    using css

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,23 @@
 import "./App.css"
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Form } from "react-bootstrap";
 
 function App() {
   return (
     <>
-
-      <div className='container'>
-        <h1 id="lg">Login Form</h1>
-        <Form>
-          <Form.Group className="mb-3">
-            <Form.Label className="label">Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label className="label">Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <Form.Group className="mb-3">
-          </Form.Group>
-          <Button variant="success" type="submit">
-            Log in
-          </Button>
-        </Form>
-      </div>
+      <div class="center">
+        <h1>Login Form</h1>
+            <form>
+              <div class="txt">
+                <label class="label">Email</label>
+              <input type="text" id="mail"></input>
+              </div>        
+              <div class="txt">
+                <label class="label">Password</label>
+              <input type="password" id="pwd" ></input>
+              </div>
+              <input type="submit" value="Log In"></input>
+            </form>
+        </div>
     </>
   )
 }
